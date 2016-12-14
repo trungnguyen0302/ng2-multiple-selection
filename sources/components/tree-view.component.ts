1b53ea4a4871c5c49f70b45d4419ba32ce39fe8f
alan.nguyen <alan.nguyen@nexcel.vn>
2016-12-14 17:27:04 +0700

    #4 - Select range with shiftKey

--- sources/components/tree-view.component.ts
+++ sources/components/tree-view.component.ts
@@ -4,9 +4,51 @@ import { ItemsSelect } from '../models/items-select';
 
 @Component({
     selector: 'tree-view',
-    templateUrl: './tree-view.component.html'
+    templateUrl: './tree-view.component.html',
+    host: {
+        '(keydown)': 'keyDown($event)',
+        '(keyup)': 'keyUp()'
+    }
 })
 
 export class TreeViewComponent {
-    @Input() items: Array<ItemsSelect>;    
+    @Input() items: Array<ItemsSelect>;
+    shiftKey: boolean;
+
+    keyDown(event: KeyboardEvent) {
+        this.shiftKey = event.shiftKey;
+    }
+
+    keyUp() {
+        this.shiftKey = false;
+    }
+
+    clickEvent(item: ItemsSelect) {
+        if (this.shiftKey && item.checked) {
+            this.checkListItem(item);
+        }
+    }
+
+    checkListItem(currentItem: ItemsSelect) {
+        var previousItem: ItemsSelect;        
+        var end = 0;
+        while (end < this.items.length && this.items[end].id !== currentItem.id) {
+            if (this.items[end].checked) {
+                previousItem = this.items[end]
+            }
+
+            end++;
+        }
+
+        var start = 0;
+        if (previousItem !== undefined) {
+            while (start < this.items.length && this.items[start].id !== previousItem.id) {
+                start++;
+            }
+        }        
+
+        for (start; start < end; start++) {
+            this.items[start].checked = true;
+        }
+    }
 }
